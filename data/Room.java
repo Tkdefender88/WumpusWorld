fad38efd4c9ca45909a8c1144f9a9c887862d779
Unknown <mfrisbee@mtech.edu>
2017-11-03 13:51:12 -0600

    did things

--- data/Room.java
+++ data/Room.java
@@ -11,20 +11,116 @@ import static helpers.StdDraw.*;
  */
 
 public class Room {
-	
+
 	private int x, y;
 	private String texture = "VisitedMapTile.png";
-	private RoomType type;
+	private Boolean stinky, breezy;
+	private Boolean gold = false;
+	private Boolean pit = false;
+	private Boolean wumpus = false;
 	
-	public Room(int x, int y, RoomType type){
+
+	public Room(int x, int y){
 		this.x = x;
 		this.y = y;
-		this.type = type;
 	}
 
-	public void draw() {
-		picture(x, y, texture);
-		System.out.printf("X: %d Y: %d\n", x ,y);
+	public layer() {
+		room[][] map = getCave();
+		for (Room[] r : map) {
+			for (Room x : r) {
+				if(r != 0) {
+					if(cave[r - 1][x].isPit ) {
+						breezy = true;
+
+					}
+					if(cave[r - 1][x].isWumpus) {
+						stinky = true;
+					} 	 
+				}
+				if(r != map.length - 1) {
+					if(cave[r + 1][x].isPit) {
+						breezy = true;
+
+					}
+					if(cave[r + 1][x].isWumpus) {
+						stinky = true;
+					} 	 
+				}
+				if(x != 0) {
+					if(cave[r][x - 1].isPit) {
+						breezy = true;
+
+					}
+					if(cave[r][x - 1].isWumpus) {
+						stinky = true;
+					} 	 
+				}
+				if(x != r.length() - 1) {
+					if(cave[r][x + 1].isPit) {
+						breezy = true;
+
+					}
+					if(cave[r][x + 1].isWumpus) {
+						stinky = true;
+					} 	 
+				}
+
+
+			}
+		}
 	}
 	
+	public setPit() {
+		this.pit = true;
+	}
+	public setGold() {
+		this.gold = true;
+	}
+	public setWumpus() {
+		this.wumpus = true;
+	}
+	
+	public boolean isPit() {
+		return pit;
+	}
+	public boolean isGold() {
+		return gold;
+	}
+	public boolean isWumpus() {
+		return wumpus;
+	}
+	public boolean isStench() {
+		return stinky;
+	}
+	public boolean isBreezy() {
+		return breezy;
+	}
+	
+	
+/*
+ * player
+ * notvisited
+ * gold
+ * wumpus
+ * stench
+ * breeze
+ * pit
+ * visited
+ */
+	public void draw() {
+		picture(x, y, texture);
+		if(isPit) {
+			picture(x, y, "PitTile.png");
+		}
+		if(isBreeze) {
+			picture(x, y, "BreezeTile.png");
+		}
+		if(isStench) {
+			picture(x, y, "StenchTile.png");
+		}
+		
+		System.out.printf("X: %d Y: %d\n", x ,y);
+	}
+
 }
