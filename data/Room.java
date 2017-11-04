d392b90a66cb03550e2380991380ff574436bb10
Unknown <mfrisbee@mtech.edu>
2017-11-04 17:44:52 -0600

    Fixed the stupid layer method i had made

--- data/Room.java
+++ data/Room.java
@@ -27,48 +27,46 @@ public class Room {
 
 	public void layer() {
 		Room[][] map = Cave.getCave();
-		for (int y = 0; y < map[0].length; y++) {
-			for (int x = 0; x < map.length; x++) {
-				if(y != 0) {
-					if(map[y - 1][x].isPit() ) {
-						breezy = true;
+		if(x != 0) {
+			if(map[x - 1][y].isPit() ) {
+				breezy = true;
 
-					}
-					if(map[y - 1][x].isWumpus()) {
-						stinky = true;
-					} 	 
-				}
-				if(y != map[0].length - 1) {
-					if(map[y + 1][x].isPit()) {
-						breezy = true;
-
-					}
-					if(map[y + 1][x].isWumpus()) {
-						stinky = true;
-					} 	 
-				}
-				if(x != 0) {
-					if(map[y][x - 1].isPit()) {
-						breezy = true;
-
-					}
-					if(map[y][x - 1].isWumpus()) {
-						stinky = true;
-					} 	 
-				}
-				if(x != map[0].length - 1) {
-					if(map[y][x + 1].isPit()) {
-						breezy = true;
-
-					}
-					if(map[y][x + 1].isWumpus()) {
-						stinky = true;
-					} 	 
-				}
 			}
+			if(map[x - 1][y].isWumpus()) {
+				stinky = true;
+			} 	 
+		}
+		if(x != map[0].length - 1) {
+			if(map[x + 1][y].isPit()) {
+				breezy = true;
+
+			}
+			if(map[x + 1][y].isWumpus()) {
+				stinky = true;
+			} 	 
+		}
+		if(y != 0) {
+			if(map[x][y - 1].isPit()) {
+				breezy = true;
+
+			}
+			if(map[y][x - 1].isWumpus()) {
+				stinky = true;
+			} 	 
+		}
+		if(y != map[0].length - 1) {
+			if(map[x][y + 1].isPit()) {
+				breezy = true;
+
+			}
+			if(map[x][y + 1].isWumpus()) {
+				stinky = true;
+			} 	 
 		}
 	}
-	
+}
+}
+
 /*
  * player
  * notvisited
@@ -79,49 +77,49 @@ public class Room {
  * pit
  * visited
  */
-	public void draw() {
-		picture(x, y, texture);
-		if(isPit()) {
-			picture(x, y, "PitTile.png");
-		}
-		System.out.println(isBreezy());
-		if(isBreezy()) {
-			picture(x, y, "BreezeTile.png");
-		}
-		if(isStench()) {
-			picture(x, y, "StenchTile.png");
-		}
-		if(isGold()) {
-			picture(x, y, "GoldTile.png");
-		} 
-		if(isWumpus()) {
-			picture(x, y, "WumpusTile.png");
-		}
+public void draw() {
+	picture(x, y, texture);
+	if(isPit()) {
+		picture(x, y, "PitTile.png");
 	}
-	
-	public void setPit() {
-		this.pit = true;
+	System.out.println(isBreezy());
+	if(isBreezy()) {
+		picture(x, y, "BreezeTile.png");
 	}
-	public void setGold() {
-		this.gold = true;
+	if(isStench()) {
+		picture(x, y, "StenchTile.png");
 	}
-	public void setWumpus() {
-		this.wumpus = true;
+	if(isGold()) {
+		picture(x, y, "GoldTile.png");
+	} 
+	if(isWumpus()) {
+		picture(x, y, "WumpusTile.png");
 	}
-	
-	public boolean isPit() {
-		return pit;
-	}
-	public boolean isGold() {
-		return gold;
-	}
-	public boolean isWumpus() {
-		return wumpus;
-	}
-	public boolean isStench() {
-		return stinky;
-	}
-	public boolean isBreezy() {
-		return breezy;
-	}
+}
+
+public void setPit() {
+	this.pit = true;
+}
+public void setGold() {
+	this.gold = true;
+}
+public void setWumpus() {
+	this.wumpus = true;
+}
+
+public boolean isPit() {
+	return pit;
+}
+public boolean isGold() {
+	return gold;
+}
+public boolean isWumpus() {
+	return wumpus;
+}
+public boolean isStench() {
+	return stinky;
+}
+public boolean isBreezy() {
+	return breezy;
+}
 }
