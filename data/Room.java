4e536cf5f57f60b57fa3dbb86f1935c89b577d45
Unknown <mfrisbee@mtech.edu>
2017-11-15 15:28:40 -0700

    Added a killWumpus method to set wumpus to false and the stink with it

--- data/Room.java
+++ data/Room.java
@@ -12,140 +12,152 @@ import static helpers.StdDraw.picture;
 
 public class Room {
 
-  private int x, y;
-  private static final int TILE_SIZE = Cave.TILE_SIZE;
-  private String texture = "VisitedMapTile.png";
-  private Boolean stinky = false;
-  private Boolean breezy = false;
-  private Boolean gold = false;
-  private Boolean pit = false;
-  private Boolean wumpus = false;
-  private Boolean isVisited = false;
+	private int x, y;
+	private static final int TILE_SIZE = Cave.TILE_SIZE;
+	private String texture = "VisitedMapTile.png";
+	private Boolean stinky = false;
+	private Boolean breezy = false;
+	private Boolean gold = false;
+	private Boolean pit = false;
+	private Boolean wumpus = false;
+	private Boolean isVisited = false;
 
-  // Room constructor.
-  public Room(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+	// Room constructor.
+	public Room(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-  /**
-   * Looks up to the map of the cave and determines if the tiles next to it are pits or the wumpus.
-   * If there is a pit or wumpus next to it then the tile becomes breezy or stinky respectively.
-   */
-  public void layer() {
-    Room[][] map = Cave.getCave();
+	/**
+	 * Looks up to the map of the cave and determines if the tiles next to it are pits or the wumpus.
+	 * If there is a pit or wumpus next to it then the tile becomes breezy or stinky respectively.
+	 */
+	public void layer() {
+		Room[][] map = Cave.getCave();
 
-    // Subtracting 50 to compensate for stdDraw drawing from center.
-    int xPos = ((x - 50) / TILE_SIZE);
-    int yPos = ((y - 50) / TILE_SIZE);
+		// Subtracting 50 to compensate for stdDraw drawing from center.
+		int xPos = ((x - 50) / TILE_SIZE);
+		int yPos = ((y - 50) / TILE_SIZE);
 
-    if (xPos != 0) {
-      if (map[xPos - 1][yPos].isPit()) {
-        breezy = true;
+		if (xPos != 0) {
+			if (map[xPos - 1][yPos].isPit()) {
+				breezy = true;
 
-      }
-      if (map[xPos - 1][yPos].isWumpus()) {
-        stinky = true;
-      }
-    }
-    if (xPos != map.length - 1) {
-      if (map[xPos + 1][yPos].isPit()) {
-        breezy = true;
+			}
+			if (map[xPos - 1][yPos].isWumpus()) {
+				stinky = true;
+			}else {
+				stinky = false;
+			}
+		}
+		if (xPos != map.length - 1) {
+			if (map[xPos + 1][yPos].isPit()) {
+				breezy = true;
 
-      }
-      if (map[xPos + 1][yPos].isWumpus()) {
-        stinky = true;
-      }
-    }
-    if (yPos != 0) {
-      if (map[xPos][yPos - 1].isPit()) {
-        breezy = true;
+			}
+			if (map[xPos + 1][yPos].isWumpus()) {
+				stinky = true;
+			}else {
+				stinky = false;
+			}
+		}
+		if (yPos != 0) {
+			if (map[xPos][yPos - 1].isPit()) {
+				breezy = true;
 
-      }
-      if (map[xPos][yPos - 1].isWumpus()) {
-        stinky = true;
-      }
-    }
-    if (yPos != map[0].length - 1) {
-      if (map[xPos][yPos + 1].isPit()) {
-        breezy = true;
+			}
+			if (map[xPos][yPos - 1].isWumpus()) {
+				stinky = true;
+			}else {
+				stinky = false;
+			}
+		}
+		if (yPos != map[0].length - 1) {
+			if (map[xPos][yPos + 1].isPit()) {
+				breezy = true;
 
-      }
-      if (map[xPos][yPos + 1].isWumpus()) {
-        stinky = true;
-      }
-    }
-  }
+			}
+			if (map[xPos][yPos + 1].isWumpus()) {
+				stinky = true;
+			}else {
+				stinky = false;
+			}
+		}
+	}
 
 
-  /**
-   * Draw method for the room. Has if statements to determine which textrures to draw over the tile
-   * depending on if it is breezy, stinky, has the wumpus or the gold or is a pit.
-   */
-  public void draw() {
-    picture(x, y, texture);
-    if (isPit()) {
-      picture(x, y, "PitTile.png");
-    }
-    if (isBreezy()) {
-      picture(x, y, "BreezeTile.png");
-    }
-    if (isStench()) {
-      picture(x, y, "StenchTile.png");
-    }
-    if (isGold()) {
-      picture(x, y, "GoldTile.png");
-    }
-    if (isWumpus()) {
-      picture(x, y, "WumpusTile.png");
-    }
-    /*
-     * if(!isVisited()) { picture(x, y, "NotVisitedMapTile.png"); }
-     */
-  }
+	/**
+	 * Draw method for the room. Has if statements to determine which textrures to draw over the tile
+	 * depending on if it is breezy, stinky, has the wumpus or the gold or is a pit.
+	 */
+	public void draw() {
+		picture(x, y, texture);
+		if (isPit()) {
+			picture(x, y, "PitTile.png");
+		}
+		if (isBreezy()) {
+			picture(x, y, "BreezeTile.png");
+		}
+		if (isStench()) {
+			picture(x, y, "StenchTile.png");
+		}
+		if (isGold()) {
+			picture(x, y, "GoldTile.png");
+		}
+		if (isWumpus()) {
+			picture(x, y, "WumpusTile.png");
+		}
+		/*
+		 * if(!isVisited()) { picture(x, y, "NotVisitedMapTile.png"); }
+		 */
+	}
 
-  /*
-   **************************************************************************
-   * GETTERS AND SETTERS
-   ************************************************************************** 
-   */
-  public void setPit() {
-    this.pit = true;
-  }
+	/*
+	 **************************************************************************
+	 * GETTERS AND SETTERS
+	 ************************************************************************** 
+	 */
+	public void setPit() {
+		this.pit = true;
+	}
 
-  public void setGold() {
-    this.gold = true;
-  }
+	public void setGold() {
+		this.gold = true;
+	}
 
-  public void setWumpus() {
-    this.wumpus = true;
-  }
+	public void setWumpus() {
+		this.wumpus = true;
+	}
 
-  public void visited() {
-    this.isVisited = true;
-  }
+	public void visited() {
+		this.isVisited = true;
+	}
 
-  public boolean isPit() {
-    return pit;
-  }
+	public void killWumpus() {
+		this.wumpus = false;
+	}
 
-  public boolean isGold() {
-    return gold;
-  }
+	public boolean isPit() {
+		return pit;
+	}
 
-  public boolean isWumpus() {
-    return wumpus;
-  }
+	public boolean isGold() {
+		return gold;
+	}
 
-  public boolean isStench() {
-    return stinky;
-  }
+	public boolean isWumpus() {
+		return wumpus;
+	}
 
-  public boolean isBreezy() {
-    return breezy;
-  }
+	public boolean isStench() {
+		return stinky;
+	}
 
-  public boolean isVisited() {
-    return isVisited;
-  }
+	public boolean isBreezy() {
+		return breezy;
+	}
+
+	public boolean isVisited() {
+		return isVisited;
+	}
 }
