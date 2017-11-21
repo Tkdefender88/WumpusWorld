c04235d7c95fddf2988ff186c3e1ff653bb6f8f8
Tkdefender88 <tkdefender88@gmail.com>
2017-11-21 13:51:59 -0700

    started implementing the new konami code method added better comments and refactored code

--- data/Player.java
+++ data/Player.java
@@ -10,7 +10,6 @@ import static helpers.StdDraw.isKeyPressed;
 import static helpers.StdDraw.picture;
 import static helpers.StdDraw.show;
 import java.awt.event.KeyEvent;
-import helpers.StdAudio;
 
 public class Player {
 
@@ -35,24 +34,6 @@ public class Player {
     draw();
     input();
   }
-
-  public int getXPos() {
-    return xPos;
-  }
-
-  public int getYPos() {
-    return yPos;
-  }
-  
-  public boolean hasGold() {
-  	return gold;
-  }
-  
-  public boolean isKonami() {
-  	return konami;
-  }
-  
-  
 
   /**
    * Draws the player to the screen.
@@ -80,89 +61,86 @@ public class Player {
       show(delay);
     } else if (isKeyPressed(KeyEvent.VK_J) && xPos > 0 && hasArrow) {
       Room.killWumpus(xPos - 1, yPos);
-      StdAudio.play("arrow2.wav");
+      // StdAudio.play("arrow2.wav");
       hasArrow = false;
       show(delay);
     } else if (isKeyPressed(KeyEvent.VK_L) && xPos < WumpusWorld.width / TILE_SIZE - 1
         && hasArrow) {
       Room.killWumpus(xPos + 1, yPos);
-      StdAudio.play("arrow2.wav");
+      // StdAudio.play("arrow2.wav");
       hasArrow = false;
       show(delay);
     } else if (isKeyPressed(KeyEvent.VK_I) && yPos < WumpusWorld.height / TILE_SIZE - 1
         && hasArrow) {
       Room.killWumpus(xPos, yPos + 1);
-      StdAudio.play("arrow2.wav");
+      // StdAudio.play("arrow2.wav");
       hasArrow = false;
       show(delay);
     } else if (isKeyPressed(KeyEvent.VK_K) && yPos > 0 && hasArrow) {
       Room.killWumpus(xPos, yPos - 1);
-      StdAudio.play("arrow2.wav");
+      // StdAudio.play("arrow2.wav");
       hasArrow = false;
       show(delay);
     } else if (isKeyPressed(KeyEvent.VK_G)) {
       Room map[][] = Cave.getCave();
       gold = map[xPos][yPos].isGold();
       show(delay);
-    } else if (isKeyPressed(KeyEvent.VK_UP)) {
-    	//System.out.println("UP");
-      if(konamiCount == 0 || konamiCount == 1) {
-      	konamiCount++;
-      }else {
-      	konamiCount = 0;
-      }
-      show(delay);
-    } else if (isKeyPressed(KeyEvent.VK_DOWN)) {
-    	//System.out.println("DOWN");
-    	if(konamiCount == 2 || konamiCount == 3) {
-      	konamiCount++;
-      }else {
-      	konamiCount = 0;
-      }
-      show(delay);
-    } else if (isKeyPressed(KeyEvent.VK_LEFT)) {
-    	//System.out.println("LEFT");
-    	if(konamiCount == 4 || konamiCount == 6) {
-      	konamiCount++;
-      }else {
-      	konamiCount = 0;
-      }
-      show(delay);
-    } else if (isKeyPressed(KeyEvent.VK_RIGHT)) {
-    	//System.out.println("RIGHT");
-    	if(konamiCount == 5 || konamiCount == 7) {
-      	konamiCount++;
-      }else {
-      	konamiCount = 0;
-      }
-      show(delay);
-    } else if (isKeyPressed(KeyEvent.VK_B)) {
-    	//System.out.println("B");
-    	if(konamiCount == 8) {
-      	konamiCount++;
-      }else {
-      	konamiCount = 0;
-      }
-      show(delay);
-    } else if (isKeyPressed(KeyEvent.VK_A)) {
-    	//System.out.println("A");
-    	if(konamiCount == 9) {
-      	konamiCount++;
-      }else {
-      	konamiCount = 0;
-      }
-      show(delay);
-    } else if (isKeyPressed(KeyEvent.VK_ENTER)) {
-    	//System.out.println("START");
-    	if(konamiCount == 10) {
-      	konami = true;
-      	System.out.println("Cheat code active.");
-      }else {
-      	konamiCount = 0;
-      }
-      show(delay);
     }
+    /*
+     * else if (isKeyPressed(KeyEvent.VK_UP)) { // System.out.println("UP"); if (konamiCount == 0 ||
+     * konamiCount == 1) { konamiCount++; } else { konamiCount = 0; } show(delay); } else if
+     * (isKeyPressed(KeyEvent.VK_DOWN)) { // System.out.println("DOWN"); if (konamiCount == 2 ||
+     * konamiCount == 3) { konamiCount++; } else { konamiCount = 0; } show(delay); } else if
+     * (isKeyPressed(KeyEvent.VK_LEFT)) { // System.out.println("LEFT"); if (konamiCount == 4 ||
+     * konamiCount == 6) { konamiCount++; } else { konamiCount = 0; } show(delay); } else if
+     * (isKeyPressed(KeyEvent.VK_RIGHT)) { // System.out.println("RIGHT"); if (konamiCount == 5 ||
+     * konamiCount == 7) { konamiCount++; } else { konamiCount = 0; } show(delay); } else if
+     * (isKeyPressed(KeyEvent.VK_B)) { // System.out.println("B"); if (konamiCount == 8) {
+     * konamiCount++; } else { konamiCount = 0; } show(delay); } else if
+     * (isKeyPressed(KeyEvent.VK_A)) { // System.out.println("A"); if (konamiCount == 9) {
+     * konamiCount++; } else { konamiCount = 0; } show(delay); } else if
+     * (isKeyPressed(KeyEvent.VK_ENTER)) { // System.out.println("START"); if (konamiCount == 10) {
+     * konami = true; System.out.println("Cheat code active."); } else { konamiCount = 0; }
+     */
+    // show(delay);
+    // }
     Room.visited(xPos, yPos);
-    // TODO: handle shooting the arrow
+  }
+
+  private boolean checkKonami(int keyCode) {
+    // Key code int values for Up up down down left right left right b a enters
+    int[] sequence = {86, 86, 40, 40, 37, 39, 37, 39, 66, 65, 61};
+    int counter = 0;
+    if (sequence[counter] == keyCode) {
+      counter += 1;
+      if (counter == sequence.length) {
+        counter = 0;
+        return true;
+      }
+    } else {
+      counter = 0;
+    }
+    return false;
+  }
+
+
+  /***************************************************************************
+   * Getters and Setters
+   ***************************************************************************/
+
+  public int getXPos() {
+    return xPos;
+  }
+
+  public int getYPos() {
+    return yPos;
+  }
+
+  public boolean hasGold() {
+    return gold;
+  }
+
+  public boolean isKonami() {
+    return konami;
   }
 }
